fecd9db534822b899c4407db54560ae65e474373
Juan Carlos Ortiz H <juanortizmg@gmail.com>
2021-07-15 12:10:33 -0500

    Retrofit with Thread Java

--- app/src/main/java/com/gitlab/juancode/coroutinesnative/MainActivity.kt
+++ app/src/main/java/com/gitlab/juancode/coroutinesnative/MainActivity.kt
@@ -1,14 +1,9 @@
 package com.gitlab.juancode.coroutinesnative
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.gitlab.juancode.coroutinesnative.databinding.ActivityMainBinding
-import com.gitlab.juancode.coroutinesnative.model.MovieResult
 import com.gitlab.juancode.coroutinesnative.server.MovieClient
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
@@ -22,20 +17,19 @@ class MainActivity : AppCompatActivity() {
         adapter = MoviesAdapter(emptyList())
         binding.recycler.adapter = adapter
 
-        val apiKey = getString(R.string.api_key)
+        Thread {
+            val apiKey = getString(R.string.api_key)
+            val listPopularMovies = MovieClient.service.listPopularMovies(apiKey)
+            val body = listPopularMovies.execute().body()
 
-        MovieClient.service.listPopularMovies(apiKey).enqueue(object : Callback<MovieResult>{
-            override fun onResponse(call: Call<MovieResult>, response: Response<MovieResult>) {
-                val body = response.body()
-                if (body != null) {
-                    adapter.movies = body.results
-                    adapter.notifyDataSetChanged()
+            runOnUiThread(object : Runnable {
+                override fun run() {
+                    if (body != null) {
+                        adapter.movies = body.results
+                        adapter.notifyDataSetChanged()
+                    }
                 }
-            }
-
-            override fun onFailure(call: Call<MovieResult>, t: Throwable) {
-                Toast.makeText(this@MainActivity, "Algo salió mal", Toast.LENGTH_SHORT).show()
-            }
-        })
+            })
+        }.start()
     }
 }
