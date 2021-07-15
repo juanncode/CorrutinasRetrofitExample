d7c9956016dd40d9a725e7817f2489c932ef49f5
Juan Carlos Ortiz H <juanortizmg@gmail.com>
2021-07-15 12:46:59 -0500

    Retrofit with lifecycleScope

--- app/src/main/java/com/gitlab/juancode/coroutinesnative/MainActivity.kt
+++ app/src/main/java/com/gitlab/juancode/coroutinesnative/MainActivity.kt
@@ -2,8 +2,12 @@ package com.gitlab.juancode.coroutinesnative
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.gitlab.juancode.coroutinesnative.databinding.ActivityMainBinding
 import com.gitlab.juancode.coroutinesnative.server.MovieClient
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
@@ -17,19 +21,14 @@ class MainActivity : AppCompatActivity() {
         adapter = MoviesAdapter(emptyList())
         binding.recycler.adapter = adapter
 
-        Thread {
+        lifecycleScope.launch {
             val apiKey = getString(R.string.api_key)
             val listPopularMovies = MovieClient.service.listPopularMovies(apiKey)
-            val body = listPopularMovies.execute().body()
-
-            runOnUiThread(object : Runnable {
-                override fun run() {
-                    if (body != null) {
-                        adapter.movies = body.results
-                        adapter.notifyDataSetChanged()
-                    }
-                }
-            })
-        }.start()
+            val body = withContext(Dispatchers.IO) { listPopularMovies.execute().body() }
+            if (body != null) {
+                adapter.movies = body.results
+                adapter.notifyDataSetChanged()
+            }
+        }
     }
 }
